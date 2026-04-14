02c87e237374f69f1ec564c89cf7d2a52113685d
Karthik <karthikkulal39@gmail.com>
2026-04-15 00:23:47 +0530

    Time: 0 ms (100%), Space: 9.2 MB (80.83%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,39 +1,43 @@
 class Solution {
-public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> vec;
-        int top = 0, bottom = matrix.size() - 1;
-        int left = 0, right = matrix[0].size() - 1;
-    
-        while(top <= bottom && left <= right){
-            // Move Right
-            for(int i = left; i <= right; i++){
-                vec.push_back(matrix[top][i]);
-            }
-            top++;
+  public: vector < int > spiralOrder(vector < vector < int >> & matrix) {
 
-            // Move Down
-            for(int i = top; i <= bottom; i++){
-                vec.push_back(matrix[i][right]);
-            }
-            right--;
+    vector < int > vec;
+    int top = 0, bottom = matrix.size() - 1;
+    int left = 0, right = matrix[0].size() - 1;
 
-            // CHECK: Does the row still exist?
-            if (top <= bottom) {
-                for(int i = right; i >= left; i--){
-                    vec.push_back(matrix[bottom][i]);
-                }
-                bottom--;
-            }
+    while (top <= bottom && left <= right) {
 
-            // CHECK: Does the column still exist?
-            if (left <= right) {
-                for(int i = bottom; i >= top; i--){
-                    vec.push_back(matrix[i][left]);
-                }
-                left++;
-            }
+      for (int i = left; i <= right; i++) {
+        vec.push_back(matrix[top][i]);
+
+      }
+      top = top + 1;
+
+      for (int i = top; i <= bottom; i++) {
+        vec.push_back(matrix[i][right]);
+
+      }
+      right = right - 1;
+
+      if (top <= bottom) {
+
+        for (int i = right; i >= left; i--) {
+          vec.push_back(matrix[bottom][i]);
+
         }
-        return vec;
+        bottom = bottom - 1;
+      }
+      if (left <= right) {
+
+        for (int i = bottom; i >= top; i--) {
+          vec.push_back(matrix[i][left]);
+
+        }
+        left = left + 1;
+      }
+
     }
+    return vec;
+
+  }
 };
